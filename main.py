fb92e9ed56c6aba9fb1c0c62c506ecdb623a28e9
PrinceXXIV <>
2021-10-08 20:21:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# This a comment - it doesn't show up on the ACTUAL screen - it is for other coders to read
+
+""" Hello How are things going G?
+"""
+
+# ==========================================================
+
+King = 30
+King = 45
+print(King)
+
+# The variable below the first will override the first variable, variable names cannot contain spaces
+
+Royal = "I am King"
+print(Royal)
+
+Los_Angeles_10 = 55
+print(Los_Angeles_10)
+
+# In python you can't have full stops or dashes and you must close paretheses
+IstheGreatest = 90
+print(IstheGreatest)
+
+# You cannot start variables with numbers - you can start with underscores
+
+XYZ = 1010
+XYZ = 2021
+print(XYZ)
+
+# In Javascript a constant variable changes when a second line is added
+
+Multiplication_10x10 = 10*10
+print(Multiplication_10x10)
+
+# In Python - Capital letters = Constant Variable. You dont want the value to change (sends a message to other coders)
+
+# Strings are words - for it to be a string you must have quotations around numbers
+
+X10X = "£222"
+print(X10X)
+
+Gen = "ten"
+print(Gen)
+
+Hundo = 100
+print(f"my number is {Hundo}")
+
+# F string allows you to pass a variable in another variable - must add an F or value won't reflect
+
